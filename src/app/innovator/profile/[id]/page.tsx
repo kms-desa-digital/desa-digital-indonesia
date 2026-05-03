5b4f273c8c9edbbf7421220da7fe859e18e5af33
muti-pan <mutiahkhairunnisabogor@gmail.com>
2026-05-03 11:54:13 +0700

    feat: update translation for innovator profile

--- src/app/innovator/profile/[id]/page.tsx
+++ src/app/innovator/profile/[id]/page.tsx
@@ -292,7 +292,7 @@ const ProfileInnovator: React.FC = () => {
                                 padding="6px 8px"
                                 borderRadius="4px"
                             >
-                                Pengajuan Inovasi
+                                {t("InnovationSubmission")}
                             </Button>
                         )}
                     </Flex>
@@ -326,7 +326,7 @@ const ProfileInnovator: React.FC = () => {
                                         paddingTop="8px"
                                     >
                                         <Box color="#4B5563" fontSize="12px" minWidth="110px">
-                                            Nomor WhatsApp
+                                            {t("whatsappNumber")}
                                         </Box>
                                         <Description>{innovatorData.whatsapp}</Description>
                                     </Flex>
@@ -338,7 +338,7 @@ const ProfileInnovator: React.FC = () => {
                                         paddingTop="12px"
                                     >
                                         <Box color="#4B5563" fontSize="12px" minWidth="110px">
-                                            Link Instagram
+                                            {t("instagramLink")}
                                         </Box>
                                         <Description>
                                             {innovatorData.instagram || "Tidak tersedia"}
@@ -352,7 +352,7 @@ const ProfileInnovator: React.FC = () => {
                                         paddingTop="12px"
                                     >
                                         <Box color="#4B5563" fontSize="12px" minWidth="110px">
-                                            Link Website
+                                            {t("websiteLink")}
                                         </Box>
                                         <Description>
                                             {innovatorData.website || "Tidak tersedia"}
